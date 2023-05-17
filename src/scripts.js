744d459b416d2aff6aeeed000c26ce763dc94729
Matias Sotelo <matysotelo07@gmail.com>
2023-05-17 16:53:26 -0300

    actualizacion desarrollo

--- src/scripts.js
+++ src/scripts.js
@@ -103,7 +103,55 @@ rubrosMenu.forEach(prod => {
       let url = "assets//img//productos//"
   
       producto.forEach( prod => {
-        contenedorCategorias.innerHTML += `<div class="card-menu"> 
+        
+        const cardMenu = document.createElement("div");
+
+        cardMenu.classList.add("card-menu");
+
+        function calcularOpciones (p) {
+
+          if (p.rubro === "pizza") {
+            
+            let opcion = `<div> 
+                            <p>Entera</p>
+                            <input type="checkbox" id="precioUnidad" value="first_checkbox">
+                            <p>$${p.precioUnidad}</p> 
+                          </div>
+                          <div> 
+                            <p>Media</p>
+                            <input type="checkbox" id="precioMedia" value="first_checkbox">
+                            <p>$${p.precioMedia}</p> 
+                          </div>
+                          `
+
+            return opcion;
+          } else if (p.rubro === "empanadas") {
+
+            let opcion = `<div> 
+                            <p>Docena</p>
+                            <input type="checkbox" id="precioUnidad" value="first_checkbox">
+                            <p>$${p.precioUnidad}</p> 
+                          </div>
+                          <div> 
+                            <p>Media</p>
+                            <input type="checkbox" id="precioMedia" value="first_checkbox">
+                            <p>$${p.precioMedia}</p> 
+                          </div>
+                          `
+
+            return opcion;
+
+          } else {
+            let opcion = `<p>$${p.precioUnidad} </p>`
+            return opcion;  
+          }
+                   
+        }
+
+        let opciones = calcularOpciones(prod);
+        
+
+        cardMenu.innerHTML = `
                                     <div class="card-img"> 
                                       <img class="menu-img" src="${url}${prod.img}.jpg"> 
                                     </div>
@@ -111,35 +159,184 @@ rubrosMenu.forEach(prod => {
                                       <h2> ${prod.nombre}</h2>
                                       <p class="descripcion">${prod.descripcion}<p>
                                       <div class="cont-btn"> 
-                                        <p> $${prod.precioUnidad}</p>
+                                        <p> ${opciones}</p>
                                         <input id="carrito-${prod.id}" type="submit" value="Agregar">
                                       </div>
                                     </div>
-                                </div>`    
+                                `   
+                              
+                                contenedorCategorias.appendChild(cardMenu)
 
-      })              
+                           
+                                // const btnCarrito = cardMenu.querySelector(`#carrito-${prod.id}`);
+                                // const checkboxUnidad = document.getElementById('precioUnidad');
+                                // const checkboxMedia = document.getElementById('precioMedia');
+                                
+                                // let nombre;
+                                // let precio;
+                                
+                                // // Agrega un event listener para detectar el clic en el checkbox de la unidad
+                                // checkboxUnidad.addEventListener('click', function() {
+                                //   if (checkboxUnidad.checked) {
+                                //     console.log('Se hizo clic en el checkbox de la unidad');
+                                //     // Asigna el nombre y precio correspondientes
+                                //     nombre = `${prod.rubro} ${prod.nombre} (Unidad)`;
+                                //     precio = `${prod.precioUnidad}`;
+                                //     checkboxMedia.checked = false; // Desmarca el checkbox de la media
+                                //   }
+                                // });
+                                
+                                // // Agrega un event listener para detectar el clic en el checkbox de la media
+                                // checkboxMedia.addEventListener('click', function() {
+                                //   if (checkboxMedia.checked) {
+                                //     console.log('Se hizo clic en el checkbox de la media');
+                                //     // Asigna el nombre y precio correspondientes
+                                //     nombre = `${prod.rubro} ${prod.nombre} (Media)`;
+                                //     precio = `${prod.precioMedia}`;
+                                //     checkboxUnidad.checked = false; // Desmarca el checkbox de la unidad
+                                //   }
+                                // });
+                                
+                                // btnCarrito.addEventListener("click", function() {
+                                //   if (nombre !== undefined && precio !== undefined) {
+                                //     let cantidad = 1;
+                                //     pedido.push({ nombre: nombre, precio: precio, cantidad: cantidad });
+                                //     console.log(pedido);
+                                //     nombre = undefined; // Reinicia el valor de nombre
+                                //     precio = undefined; // Reinicia el valor de precio
+                                //     checkboxUnidad.checked = false; // Desmarca el checkbox de la unidad
+                                //     checkboxMedia.checked = false; // Desmarca el checkbox de la media
+                                //   } else {
+                                //     console.log('No se ha seleccionado ninguna opción');
+                                //   }
+                                // });
 
-      contenedorCategorias.addEventListener('click', function(event) {
-        const target = event.target;
+                                const btnCarrito = cardMenu.querySelector(`#carrito-${prod.id}`);
         
-        if (target.matches('input[type="submit"]')) {
-          // El evento corresponde a un botón de agregar
-          const id = target.id.split('-')[1]; // Obtener el ID del producto
+        // const checkboxUnidad = document.getElementById('precioUnidad');
+        // const checkboxMedia = document.getElementById('precioMedia');
 
-          // Realizar las acciones necesarias
-          // console.log((id - 1))
+        // let nombre;
+        // let precio;
 
-          pedido.push(producto[(id-1)])
+        // // Agrega un event listener para detectar el clic en el checkbox de la unidad
+        // checkboxUnidad.addEventListener('click', function() {
+        //   if (checkboxUnidad.checked) {
+        //     console.log('Se hizo clic en el checkbox de la unidad');
+        //     // Asigna el nombre y precio correspondientes
+        //     nombre = `${prod.rubro} ${prod.nombre} (Unidad)`;
+        //     precio = `${prod.precioUnidad}`;
+        //     checkboxMedia.checked = false; // Desmarca el checkbox de la media
+        //   }
+        // });
 
-          // console.log(pedido)
-          const contador = document.querySelector("#contador");
-          contador.textContent++;
-          contador.style.display = "inline";
+        // // Agrega un event listener para detectar el clic en el checkbox de la media
+        // checkboxMedia.addEventListener('click', function() {
+        //   if (checkboxMedia.checked) {
+        //     console.log('Se hizo clic en el checkbox de la media');
+        //     // Asigna el nombre y precio correspondientes
+        //     nombre = `${prod.rubro} ${prod.nombre} (Media)`;
+        //     precio = `${prod.precioMedia}`;
+        //     checkboxUnidad.checked = false; // Desmarca el checkbox de la unidad
+        //   }
+        // });
 
-          console.log(pedido);
+        // btnCarrito.addEventListener("click", function() {
+        //   let cantidad = 1;
+        //   if (nombre && precio) {
+        //     pedido.push({ nombre: nombre, precio: precio, cantidad: cantidad });
+        //     console.log(pedido);
+        //     nombre = undefined; // Reinicia el valor de nombre
+        //     precio = undefined; // Reinicia el valor de precio
+        //     checkboxUnidad.checked = false; // Desmarca el checkbox de la unidad
+        //     checkboxMedia.checked = false; // Desmarca el checkbox de la media
+        //   } else {
+        //     nombre = `${prod.rubro} ${prod.nombre}`;
+        //     precio = `${prod.precioUnidad}`;
+        //     pedido.push({ nombre: nombre, precio: precio, cantidad: cantidad });
+        //     console.log(pedido);
+        //     nombre = undefined; // Reinicia el valor de nombre
+        //     precio = undefined; // Reinicia el valor de precio
+        //   }
+        // });
 
+        const checkboxUnidad = document.getElementById('precioUnidad');
+        const checkboxMedia = document.getElementById('precioMedia');
+        
+        let nombre;
+        let precio;
+        
+        // Verifica si checkboxUnidad existe antes de agregar el event listener
+        if (checkboxUnidad) {
+          checkboxUnidad.addEventListener('click', function() {
+            if (checkboxUnidad.checked) {
+              console.log('Se hizo clic en el checkbox de la unidad');
+              // Asigna el nombre y precio correspondientes
+              nombre = `${prod.rubro} ${prod.nombre} (Unidad)`;
+              precio = `${prod.precioUnidad}`;
+              checkboxMedia.checked = false; // Desmarca el checkbox de la media
+            }
+          });
         }
-      });
+        
+        // Agrega el event listener solo si checkboxMedia existe
+        if (checkboxMedia) {
+          checkboxMedia.addEventListener('click', function() {
+            if (checkboxMedia.checked) {
+              console.log('Se hizo clic en el checkbox de la media');
+              // Asigna el nombre y precio correspondientes
+              nombre = `${prod.rubro} ${prod.nombre} (Media)`;
+              precio = `${prod.precioMedia}`;
+              checkboxUnidad.checked = false; // Desmarca el checkbox de la unidad
+            }
+          });
+        }
+        
+        btnCarrito.addEventListener("click", function() {
+          let cantidad = 1;
+          if (nombre && precio) {
+            pedido.push({ nombre: nombre, precio: precio, cantidad: cantidad });
+            console.log(pedido);
+            nombre = undefined; // Reinicia el valor de nombre
+            precio = undefined; // Reinicia el valor de precio
+            if (checkboxUnidad) checkboxUnidad.checked = false; // Desmarca el checkbox de la unidad si existe
+            if (checkboxMedia) checkboxMedia.checked = false; // Desmarca el checkbox de la media si existe
+          } else {
+            nombre = `${prod.rubro} ${prod.nombre}`;
+            precio = `${prod.precioUnidad}`;
+            pedido.push({ nombre: nombre, precio: precio, cantidad: cantidad });
+            console.log(pedido);
+            nombre = undefined; // Reinicia el valor de nombre
+            precio = undefined; // Reinicia el valor de precio
+          }
+        });
+                        
+
+
+                                
+      })   
+
+      
+
+      
+      // contenedorCategorias.addEventListener('click', function(event) {
+      //   const target = event.target;
+        
+      //   if (target.matches('input[type="submit"]')) {
+      //     // El evento corresponde a un botón de agregar
+      //     const id = target.id.split('-')[1]; // Obtener el ID del producto
+
+      //     pedido.push(producto[(id-1)])
+
+      //     // console.log(pedido)
+      //     const contador = document.querySelector("#contador");
+      //     contador.textContent = "✓";
+      //     contador.style.display = "inline";
+
+      //     console.log(pedido);
+
+      //   }
+      // });
              
       
 
@@ -157,6 +354,7 @@ rubrosMenu.forEach(prod => {
 // mostrarPedidoEnDom
 const contPedido = document.querySelector(".logo-pedido");
 const contenedorPedido = document.querySelector(".contenedor-pedido");
+  
 
 contPedido.addEventListener("click", function(){
   contenedorPedido.style.display = "block";
@@ -171,10 +369,39 @@ contPedido.addEventListener("click", function(){
   
   contenedorPedido.appendChild(headerCategoDom);
 
-  // pedido.forEach( prod => {
-  //   contenedorPedido.innerHTML = `asd`
-  //   contenedorPedido.appendChild()
-  // })
+  // Mostrar pedido en carrito
+  pedido.forEach( prod => {
+
+    const cardPedido = document.createElement("div");
+    cardPedido.classList.add("card-pedido");
+
+    // img:
+    // rubro: 
+    // nombre: 
+    // descripcion: 
+    // precioUnidad:
+    // precioMedia:
+
+    // if(prod.precioU === undefined ) {
+    //   let precios = `
+    //   <input type="checkbox" value="figazzas lomo"> <p>$${prod.precioM}</>
+    //   <p>$${prod.precioU}</>
+    //   `
+    // } 
+    
+    cardPedido.innerHTML = `
+                          
+                            <h2>${prod.nombre}</h2>
+                            
+                            <p>$${prod.precio}</>
+                            
+
+      
+      `
+    
+    contenedorPedido.appendChild(cardPedido);
+    console.log("hola?")
+  })
 
   const btnCerrar = document.querySelector(".btn-cerrar-menu");
 
@@ -185,7 +412,7 @@ contPedido.addEventListener("click", function(){
 
 })
 
-
+  
 
 
 
